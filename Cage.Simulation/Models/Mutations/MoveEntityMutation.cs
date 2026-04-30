33f54c39a518c466632f06d75737e47a48023ba0
Nick Pruehs <npruehs@outlook.com>
2026-04-30 23:35:28 +0200

    Add mutations for shuffling entity lists and moving entities between lists.

---
+++ Cage.Simulation/Models/Mutations/MoveEntityMutation.cs
@@ -0,0 +1,65 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Cage.Simulation.Models;
+using Cage.Simulation.Models.Expressions;
+
+namespace Cage.Simulation.Models.Mutations;
+
+public sealed class MoveEntityMutation : Mutation
+{
+    public Expression EntityToMove { get; set; } = null!;
+    public Expression SourceEntity { get; set; } = null!;
+    public string SourceAttribute { get; set; } = null!;
+    public Expression TargetEntity { get; set; } = null!;
+    public string TargetAttribute { get; set; } = null!;
+
+    public override void Apply(Match match, EvaluationContext context)
+    {
+        var entityToMove = EntityToMove.Evaluate(context).AsEntity();
+        var sourceEntity = SourceEntity.Evaluate(context).AsEntity();
+        var targetEntity = TargetEntity.Evaluate(context).AsEntity();
+
+        var sourceList = sourceEntity.Attributes[SourceAttribute].AsEntityList();
+        var targetList = targetEntity.Attributes[TargetAttribute].AsEntityList();
+
+        // Remove from source list
+        sourceList.Remove(entityToMove);
+
+        // Add to target list
+        targetList.Add(entityToMove);
+    }
+
+    internal override void WriteToJson(Utf8JsonWriter writer, JsonSerializerOptions options)
+    {
+        writer.WritePropertyName("entityToMove");
+        JsonSerializer.Serialize(writer, EntityToMove, options);
+
+        writer.WritePropertyName("sourceEntity");
+        JsonSerializer.Serialize(writer, SourceEntity, options);
+
+        writer.WriteString("sourceAttribute", SourceAttribute);
+
+        writer.WritePropertyName("targetEntity");
+        JsonSerializer.Serialize(writer, TargetEntity, options);
+
+        writer.WriteString("targetAttribute", TargetAttribute);
+    }
+
+    internal override void ReadFromJson(JsonElement root, JsonSerializerOptions options)
+    {
+        if (root.TryGetProperty("entityToMove", out var entityToMoveProp))
+            EntityToMove = JsonSerializer.Deserialize<Expression>(entityToMoveProp.GetRawText(), options) ?? throw new JsonException("Failed to deserialize entityToMove expression");
+
+        if (root.TryGetProperty("sourceEntity", out var sourceEntityProp))
+            SourceEntity = JsonSerializer.Deserialize<Expression>(sourceEntityProp.GetRawText(), options) ?? throw new JsonException("Failed to deserialize sourceEntity expression");
+
+        if (root.TryGetProperty("sourceAttribute", out var sourceAttributeProp))
+            SourceAttribute = sourceAttributeProp.GetString() ?? throw new JsonException("Missing sourceAttribute value");
+
+        if (root.TryGetProperty("targetEntity", out var targetEntityProp))
+            TargetEntity = JsonSerializer.Deserialize<Expression>(targetEntityProp.GetRawText(), options) ?? throw new JsonException("Failed to deserialize targetEntity expression");
+
+        if (root.TryGetProperty("targetAttribute", out var targetAttributeProp))
+            TargetAttribute = targetAttributeProp.GetString() ?? throw new JsonException("Missing targetAttribute value");
+    }
+}
---
+++ Cage.Simulation/Models/Mutations/ShuffleEntityListMutation.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Cage.Simulation.Models;
+using Cage.Simulation.Models.Expressions;
+
+namespace Cage.Simulation.Models.Mutations;
+
+public sealed class ShuffleEntityListMutation : Mutation
+{
+    public Expression Entity { get; set; } = null!;
+    public string Attribute { get; set; } = null!;
+
+    public override void Apply(Match match, EvaluationContext context)
+    {
+        var entity = Entity.Evaluate(context).AsEntity();
+        var list = entity.Attributes[Attribute].AsEntityList();
+
+        // Fisher-Yates shuffle
+        var random = new Random();
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int j = random.Next(i + 1);
+            (list[i], list[j]) = (list[j], list[i]);
+        }
+    }
+
+    internal override void WriteToJson(Utf8JsonWriter writer, JsonSerializerOptions options)
+    {
+        writer.WritePropertyName("entity");
+        JsonSerializer.Serialize(writer, Entity, options);
+
+        writer.WriteString("attribute", Attribute);
+    }
+
+    internal override void ReadFromJson(JsonElement root, JsonSerializerOptions options)
+    {
+        if (root.TryGetProperty("entity", out var entityProp))
+            Entity = JsonSerializer.Deserialize<Expression>(entityProp.GetRawText(), options) ?? throw new JsonException("Failed to deserialize entity expression");
+
+        if (root.TryGetProperty("attribute", out var attributeProp))
+            Attribute = attributeProp.GetString() ?? throw new JsonException("Missing attribute value");
+    }
+}
