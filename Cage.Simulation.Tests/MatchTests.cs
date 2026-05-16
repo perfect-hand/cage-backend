a25ebc18e068041419350f8383ee9ae90b36ebb2
Nick Pruehs <npruehs@outlook.com>
2026-05-16 22:10:48 +0200

    Add tests for recently added code.

--- Cage.Simulation.Tests/MatchTests.cs
+++ Cage.Simulation.Tests/MatchTests.cs
@@ -1,6 +1,11 @@
 
+using System.Linq.Expressions;
 using Cage.Simulation.Models;
 using Cage.Simulation.Models.Entities;
+using Cage.Simulation.Models.Events;
+using Cage.Simulation.Models.Expressions;
+using Cage.Simulation.Models.Functions;
+using Cage.Simulation.Models.Mutations;
 using Cage.Simulation.Models.Types;
 
 namespace Cage.Simulation.Tests;
@@ -8,7 +13,7 @@ namespace Cage.Simulation.Tests;
 public class MatchTests
 {
     [Fact]
-    public void SimulateWholeMatch()
+    public void InitializesMatch()
     {
         // Setup data.
         var resourceCard = new EntityBlueprint("Resource");
@@ -31,17 +36,121 @@ public class MatchTests
         var deckCardIds = new List<string> { "Resource", "Resource", "Resource", "Resource", "Monster", "Monster", "Monster", "Monster" };
 
         var player1 = match.EntityManager.CreateEntity();
+        player1.Tags.Add("Player");
         var player1Deck = entityManager.CreateEntitiesFromBlueprintIds(deckCardIds);
         player1.Attributes["Deck"] = new TypedValue(player1Deck.ToList());
+        player1.Attributes["Hand"] = new TypedValue(new List<Entity>());
 
         var player2 = match.EntityManager.CreateEntity();
+        player2.Tags.Add("Player");
         var player2Deck = entityManager.CreateEntitiesFromBlueprintIds(deckCardIds);
         player2.Attributes["Deck"] = new TypedValue(player2Deck.ToList());
+        player2.Attributes["Hand"] = new TypedValue(new List<Entity>());
 
-        // TODO: Add fuction for getting first entity in a list.
-        // TODO: Add event for match initialization.
-        // TODO: Add trigger listening for match initialization.
-        // TODO: Shuffle decks, draw hands, set initial life.
-        // TODO: Add trigger listening for health changes, and raising victory and defeat events.
+        // Build trigger for match initialization.
+        var matchInitializationTrigger = new Trigger
+        {
+            EventName = typeof(MatchInitializationEvent).Name,
+            Actions =
+            {
+                new ForEachEntityMutation
+                {
+                    LoopVariable = "player",
+                    EntityList = new FunctionExpression
+                    {
+                        Function = new FindEntitiesByTagFunction
+                        {
+                            Tag = new LiteralExpression
+                            {
+                                Value = new TypedValue("Player")
+                            }
+                        }
+                    },
+                    Mutations =
+                    {
+                        new ShuffleEntityListMutation
+                        {
+                            Entity = new VariableExpression
+                            {
+                                Name = "player"
+                            },
+                            Attribute = "Deck"
+                        },
+                        new RepeatMutation
+                        {
+                            Times = new LiteralExpression
+                            {
+                                Value = new TypedValue(3)
+                            },
+                            Mutations =
+                            {
+                                new MoveEntityMutation
+                                {
+                                    EntityToMove = new FunctionExpression
+                                    {
+                                        Function = new EntityAtIndexFunction
+                                        {
+                                            EntityList = new FunctionExpression
+                                            {
+                                                Function = new EntityAttributeValueFunction
+                                                {
+                                                    Entity = new VariableExpression
+                                                    {
+                                                        Name = "player"
+                                                    },
+                                                    AttributeName = new LiteralExpression
+                                                    {
+                                                        Value = new TypedValue("Deck")
+                                                    }
+                                                }
+                                            },
+                                            ListIndex = new LiteralExpression
+                                            {
+                                                Value = new TypedValue(0)
+                                            }
+                                        }
+                                    },
+                                    SourceEntity = new VariableExpression
+                                    {
+                                        Name = "player"
+                                    },
+                                    SourceAttribute = "Deck",
+                                    TargetEntity = new VariableExpression
+                                    {
+                                        Name = "player"
+                                    },
+                                    TargetAttribute = "Hand"
+                                }
+                            }
+                        },
+                        new SetIntegerAttributeMutation
+                        {
+                            Entity = new VariableExpression
+                            {
+                                Name = "player"
+                            },
+                            Attribute = "Health",
+                            NewValue = new LiteralExpression
+                            {
+                                Value = new TypedValue(10)
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        // Raise match initialization event.
+        var eventManager = new EventManager();
+        eventManager.HandleEvent(new MatchInitializationEvent(), new[] { matchInitializationTrigger }, entityManager);
+
+        // Assert expected results.
+        Assert.Equal(5, player1.Attributes["Deck"].AsEntityList().Count);
+        Assert.Equal(3, player1.Attributes["Hand"].AsEntityList().Count);
+        Assert.Equal(10, player1.Attributes["Health"].AsInt());
+
+        Assert.Equal(5, player2.Attributes["Deck"].AsEntityList().Count);
+        Assert.Equal(3, player2.Attributes["Hand"].AsEntityList().Count);
+        Assert.Equal(10, player2.Attributes["Health"].AsInt());
     }
 }
